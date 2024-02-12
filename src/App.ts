64b12a24f3442a08393f4f3dec2e5c3e7be31288
Yan Avlis <ccyan05@alu.ufc.br>
2024-02-12 10:18:26 -0300

    src/App.ts
    App.ts

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,21 @@
-function greetings(name: string): string {
-    return `Hello World, ${name}`;
+import HttpServer from "./http/http-server";
+import UsersController from "./controllers/users";
+
+class App {
+	readonly httpServer = new HttpServer();
+
+	readonly usersController = new UsersController(this.httpServer);
+
+	listen(port: number) {
+		this.httpServer.listen(port, () => {
+			console.log("Server is running on port 4000");
+		});
+	}
+
+	close() {
+		this.httpServer.close();
+	}
 }
 
-console.log(greetings("User"));
+const app = new App();
+app.listen(4000);
