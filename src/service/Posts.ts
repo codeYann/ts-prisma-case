a6a76af53482b3c2ca1d82cfae4576e5651188f4
Yan Avlis <ccyan05@alu.ufc.br>
2024-02-22 14:27:29 -0300

    src/service
    Creating Posts' service

---
+++ src/service/Posts.ts
@@ -0,0 +1,43 @@
+import IPostRepository from "../repository/IPosts";
+
+export default class PostsService<T> {
+    constructor(private readonly repository: IPostRepository<T>) {
+        this.repository = repository;
+    }
+
+    async getPosts(): Promise<T[]> {
+        try {
+            const posts = await this.repository.getPosts();
+            return posts;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async storePosts(post: T): Promise<T> {
+        try {
+            const newPost = await this.repository.storePost(post);
+            return newPost;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updatePost(uuid: string, post: Partial<T>): Promise<T> {
+        try {
+            const result = await this.repository.updatePost(uuid, post);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deletePost(uuid: string): Promise<T> {
+        try {
+            const result = await this.repository.deletePost(uuid);
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
