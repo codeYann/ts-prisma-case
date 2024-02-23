e06904bfa6f8f9d8544495b17ff0d8def04d3c8a
Yan Avlis <ccyan05@alu.ufc.br>
2024-02-23 11:46:01 -0300

    src/service/Posts.ts
    Adding getPostsFromUser method

--- src/service/Posts.ts
+++ src/service/Posts.ts
@@ -1,43 +1,52 @@
 import IPostRepository from "../repository/IPosts";
 
 export default class PostsService<T> {
-    constructor(private readonly repository: IPostRepository<T>) {
-        this.repository = repository;
-    }
+	constructor(private readonly repository: IPostRepository<T>) {
+		this.repository = repository;
+	}
 
-    async getPosts(): Promise<T[]> {
-        try {
-            const posts = await this.repository.getPosts();
-            return posts;
-        } catch (error) {
-            throw error;
-        }
-    }
+	async getPosts(): Promise<T[]> {
+		try {
+			const posts = await this.repository.getPosts();
+			return posts;
+		} catch (error) {
+			throw error;
+		}
+	}
 
-    async storePosts(post: T): Promise<T> {
-        try {
-            const newPost = await this.repository.storePost(post);
-            return newPost;
-        } catch (error) {
-            throw error;
-        }
-    }
+	async getPostsFromUser(uuid: string): Promise<Partial<T>[]> {
+		try {
+			const posts = await this.repository.getPostsFromUser(uuid);
+			return posts;
+		} catch (error) {
+			throw error;
+		}
+	}
 
-    async updatePost(uuid: string, post: Partial<T>): Promise<T> {
-        try {
-            const result = await this.repository.updatePost(uuid, post);
-            return result;
-        } catch (error) {
-            throw error;
-        }
-    }
+	async storePosts(post: T): Promise<T> {
+		try {
+			const newPost = await this.repository.storePost(post);
+			return newPost;
+		} catch (error) {
+			throw error;
+		}
+	}
 
-    async deletePost(uuid: string): Promise<T> {
-        try {
-            const result = await this.repository.deletePost(uuid);
-            return result;
-        } catch (error) {
-            throw error;
-        }
-    }
+	async updatePost(uuid: string, post: Partial<T>): Promise<T> {
+		try {
+			const result = await this.repository.updatePost(uuid, post);
+			return result;
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async deletePost(uuid: string): Promise<T> {
+		try {
+			const result = await this.repository.deletePost(uuid);
+			return result;
+		} catch (error) {
+			throw error;
+		}
+	}
 }
